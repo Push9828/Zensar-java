3e5dfa9c294d36203b5825e231a09a8f1c15c40d
Pushkar Thakur <31934448+Push9828@users.noreply.github.com>
2020-01-07 16:43:54 +0530

    JUnit Demo and testing stuff

---
+++ JUnitDemo/src/Test1.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+
+@BeforeEach
+void setUp() throws Exception {
+}
+
+@Test
+void test() {
+System.out.println("Test 1");
+fail("Not yet implemented");
+}
+
+}
---
+++ JUnitDemo/src/Test2.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+ public class Test2 {
+
+@BeforeEach
+void setUp() throws Exception {
+}
+
+@Test
+void test() {
+System.out.println("Test 2");
+fail("Not yet implemented");
+}
+
+}
